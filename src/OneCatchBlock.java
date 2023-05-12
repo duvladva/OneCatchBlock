6f16a53f3f52874c1559fc4d9701b44427a65149
Владимир <duvla@mail.ru>
2023-05-12 17:15:30 +1000

    HW Task 7

--- src/OneCatchBlock.java
+++ src/OneCatchBlock.java
@@ -14,6 +14,7 @@ public class OneCatchBlock {
 
         } catch (ArithmeticException | MyException e) {
             System.out.println("Нужно обработать исключение ArithmeticException или MyException");
+            System.out.println(e);
 
 
         }
